ae0f0aa22c0d88aaaa615f22b101d50b6192a980
Igor de Lima Nunes <igorlnunes@gmail.com>
2024-02-17 10:11:18 -0300

    Config TypeORM & Create first table

---
+++ 1708133103944-Teste.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Teste1708133103944 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
